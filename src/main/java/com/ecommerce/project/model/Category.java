f60a2651ed750d5b7c3037d752a72420683a71eb
Ilias <ilias.azagagh@icloud.com>
2024-08-09 16:34:47 +0200

    Configured category class to use LOMBOK

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -4,11 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // De class die Sping Boot als een entiteit ziet. Het betreft een POJO (plain old java object) die een representatie is
 // van een table in de database. De attributes zijn de columns van de table. Elke instantie van dit object is een rij in
 // de tabel
 @Entity
+// Lombok annotation om de getters/setters/toString en meer te vervangen
+// Elk attribuut heeft een getter & setter nodig. Anders kunnen de attributen niet gezet worden in de database en
+// ook niet opgehaald worden.
+@Data
+// Bij het bouwen van een lege database worden de entiteiten geïnstantieerd. Wanneer dit gebeurt, zijn er geen
+// properties die meegegeven worden om de attributen mee te vullen. Daarom is een lege constructor nodig
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     // Omdat de gebruiker niet in controle mag zijn bij het definieren van een ID, moet deze automatisch gegenereerd
     // worden. De auto strategy is de meest veilige strategie voor verschillende DBMS. Auto genereert automatisch IDs
@@ -17,32 +28,4 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long categoryId;
     private String categoryName;
-
-    // Bij het bouwen van een lege database worden de entiteiten geïnstantieerd. Wanneer dit gebeurt, zijn er geen
-    // properties die meegegeven worden om de attributen mee te vullen. Daarom is een lege constructor nodig
-    public Category() {
-    }
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    // Elk attribuut heeft een getter & setter nodig. Anders kunnen de attributen niet gezet worden in de database en
-    // ook niet opgehaald worden.
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
