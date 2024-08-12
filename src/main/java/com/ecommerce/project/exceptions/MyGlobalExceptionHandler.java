180931b2cccc4fc61c8fad7e8956f68afda053fc
Ilias <ilias.azagagh@icloud.com>
2024-08-12 12:47:42 +0200

    Created GlobalExceptionHandler

---
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.ecommerce.project.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// De annotation onderschept elke exception die door de controllers wordt gegooid.
+// De onderschepte exceptions worden afgehandeld door deze class
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    // De annotation beschrijft dat de methode een exception handler is van het type "MethodArgumentNotValidException"
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        // Map interface wordt gebruikt om een representatie te maken van een key-value pair.
+        // Omdat Map een interface is, kan je daar geen object van maken. Daarom wordt een hashmap gemaakt
+        // (deze implementeert de Map interface)
+        // De eerste String betreft de veldnaam (key), de tweede de error message (value)
+        // Beide values betreffen objecten
+        Map<String, String> response = new HashMap<>();
+
+        // Voor elke exception, haal de veldnaam en de error message op en sla deze op in de HashMap
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError)err).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+
+        // ResponseEntity is een class binnen Spring die de HTTP-response representeert
+        // (Status code, headers en body)
+        // Dit maakt het mogelijk om de HTTP-responses te configureren
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
