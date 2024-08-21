a41dc3261424b3d5f8bded23e0e44025987d5574
Ilias <ilias.azagagh@icloud.com>
2024-08-21 16:24:14 +0200

    Added requestparameters for pagination

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.product.ProductResponse;
 import com.ecommerce.project.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +18,12 @@ public class ProductController {
     IProductService productService;
 
     @GetMapping("public/products")
-    public ResponseEntity<ProductResponse> getAllProducts() {
+    public ResponseEntity<ProductResponse> getAllProducts(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER) int pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE) int pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_BY) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR) String sortDir
+    ) {
         ProductResponse productResponse = productService.getAllProducts();
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
