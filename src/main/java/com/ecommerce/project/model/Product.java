c790b0e9b35e1aa17602531f68818db77960df40
Ilias <ilias.azagagh@icloud.com>
2024-08-21 15:35:30 +0200

    Created product layers

---
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,28 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+    private String productName;
+    private String description;
+    private int quantity;
+    @NotNull
+    private double price;
+    @NotNull
+    private double specialPrice;
+    @ManyToOne
+    private Long categoryId;
+
+}
