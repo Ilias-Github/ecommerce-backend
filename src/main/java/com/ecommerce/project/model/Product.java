452f8150f572b5347895196fb87725b957d61ec0
Ilias <ilias.azagagh@icloud.com>
2024-08-23 14:48:02 +0200

    Added jsonignore to fix recursion

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -25,6 +26,7 @@ public class Product {
     @NotNull
     private double specialPrice;
     @ManyToOne
+    @JsonIgnore
     private Category category;
 
 }
