5dfc198ea378896298e34bcf98f4f549063cc39b
Ilias <ilias.azagagh@icloud.com>
2024-08-25 08:50:00 +0200

    Moved file upload to seperate service for use in the whole project

---
+++ src/main/java/com/ecommerce/project/service/FileServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ecommerce.project.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileServiceImpl implements IFileService {
+    @Override
+    public String uploadImage(String path, MultipartFile file) throws IOException {
+        // De bestandsnaam is nodig om te achterhalen welke extensie de file heeft
+        String originalFileName = file.getOriginalFilename();
+
+        // Genereer een random unieke file naam zodat deze geen files in de database gaat overschrijven
+        // UUID genereert een random string die zo lang en random is dat het vrijwel onmogelijk is dat het twee keer
+        // dezelfde string genereert. Omdat de kans op duplicate namen extreem klein is, is het niet nodig om te
+        // checken of de naam al bestaat
+        String randomId = UUID.randomUUID().toString();
+        // Hier wordt de extensie van de originele filenaam toegevoegd aan de randomId. De extensie is altijd de
+        // laatste characters na de laatste "." in een filenaam
+        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+        // File.separator wordt gebruikt omdat separators OS afhankelijk zijn. Deze wil je dus nooit hard coded hebben
+        String filePath = path + File.separator + fileName;
+
+        // Check of de directory bestaan. Maak deze aan indien het niet het geval is
+        File folder = new File(path);
+        if (!folder.exists())
+            folder.mkdir();
+
+        // Kopieer het bestand naar de door ons aangegeven file path
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+
+        return fileName;
+    }
+}
--- src/main/java/com/ecommerce/project/service/product/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/product/ProductServiceImpl.java
@@ -8,17 +8,15 @@ import com.ecommerce.project.payload.product.ProductDTO;
 import com.ecommerce.project.payload.product.ProductResponse;
 import com.ecommerce.project.repositories.ICategoryRepository;
 import com.ecommerce.project.repositories.IProductRepository;
+import com.ecommerce.project.service.IFileService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.UUID;
 
 import static java.lang.Math.round;
 
@@ -30,6 +28,10 @@ public class ProductServiceImpl implements IProductService {
     private ICategoryRepository categoryRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private IFileService fileService;
+    @Value("${project.image}")
+    private String path;
 
     @Override
     public ProductDTO createProduct(ProductDTO productDTO, Long categoryId) {
@@ -132,10 +134,9 @@ public class ProductServiceImpl implements IProductService {
 
         // Path waar de plaatjes opgeslagen worden op de server. Dit is op de root van de server. Een directory die
         // naast de src directory bestaat
-        String path = "images";
 
         // Sla de bestandsnaam op die teruggegeven wordt na het succesvol uploaden van een image
-        String fileName = uploadImage(path, image);
+        String fileName = fileService.uploadImage(path, image);
 
         // Sla de naam van de afbeelding op in het object
         product.setImage(fileName);
@@ -143,32 +144,6 @@ public class ProductServiceImpl implements IProductService {
         Product updatedProduct = productRepository.save(product);
 
         return modelMapper.map(updatedProduct, ProductDTO.class);
-    }
-
-    private String uploadImage(String path, MultipartFile file) throws IOException {
-        // De bestandsnaam is nodig om te achterhalen welke extensie de file heeft
-        String originalFileName = file.getOriginalFilename();
-
-        // Genereer een random unieke file naam zodat deze geen files in de database gaat overschrijven
-        // UUID genereert een random string die zo lang en random is dat het vrijwel onmogelijk is dat het twee keer
-        // dezelfde string genereert. Omdat de kans op duplicate namen extreem klein is, is het niet nodig om te
-        // checken of de naam al bestaat
-        String randomId = UUID.randomUUID().toString();
-        // Hier wordt de extensie van de originele filenaam toegevoegd aan de randomId. De extensie is altijd de
-        // laatste characters na de laatste "." in een filenaam
-        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
-        // File.separator wordt gebruikt omdat separators OS afhankelijk zijn. Deze wil je dus nooit hard coded hebben
-        String filePath = path + File.separator + fileName;
-
-        // Check of de directory bestaan. Maak deze aan indien het niet het geval is
-        File folder = new File(path);
-        if (!folder.exists())
-            folder.mkdir();
-
-        // Kopieer het bestand naar de door ons aangegeven file path
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-
-        return fileName;
     }
 
     private double SpecialPriceCalculation(double price, double discount) {
