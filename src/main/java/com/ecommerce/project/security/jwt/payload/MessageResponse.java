763b3b4b107dce2fabd2abcf460868a413977f30
Ilias <ilias.azagagh@icloud.com>
2024-08-30 10:16:35 +0200

    Created message response dto

---
+++ src/main/java/com/ecommerce/project/security/jwt/payload/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project.security.jwt.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
