c148b61dadd940749547b8592304b918f1a6e6b5
Ilias <ilias.azagagh@icloud.com>
2024-09-01 09:25:31 +0200

    Updated repositories with custom queries

--- src/main/java/com/ecommerce/project/repositories/ICartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/ICartItemRepository.java
@@ -2,6 +2,11 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ICartItemRepository extends JpaRepository<CartItem, Long> {
+    // Custom query geschreven omdat product id niet gevonden werd
+    // TODO: kan dit zonder custom query?
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    CartItem findCartItemByCartIdAndProductId(Long cartId, Long productId);
 }
