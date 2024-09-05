57b1292b1db47ebe1300689a2786c7eb948bbb35
Ilias <ilias.azagagh@icloud.com>
2024-09-05 10:20:57 +0200

    AddressDTO

---
+++ src/main/java/com/ecommerce/project/payload/AddressDTO.java
@@ -0,0 +1,22 @@
+package com.ecommerce.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDTO {
+    private Long addressId;
+
+    private String streetName;
+
+    private int houseNumber;
+
+    private String apartmentNumber;
+
+    private String zipcode;
+
+    private String city;
+}
