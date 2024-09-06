dc3741595d3b352b10d035a48d149a270ab6b122
Ilias <ilias.azagagh@icloud.com>
2024-09-06 14:51:08 +0200

    Created order and order item models

---
+++ src/main/java/com/ecommerce/project/model/Order.java
@@ -0,0 +1,36 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // TODO: is dit veld nodig gezien een order al gekoppeld is aan een user
+    @Email
+    @Column
+    private String email;
+
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<OrderItem> orderItemList;
+
+    private LocalDate orderDate;
+
+    private double totalAmount;
+    private String orderStatus;
+
+    @ManyToOne
+    private Address address;
+}
---
+++ src/main/java/com/ecommerce/project/model/OrderItem.java
@@ -0,0 +1,26 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Order order;
+
+    private int quantity;
+    private double discount;
+    private double orderedProductPrice;
+}
