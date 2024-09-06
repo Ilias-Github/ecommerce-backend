f1c7afdfb271ef7533b91b0ba24f7e101f3cf5aa
Ilias <ilias.azagagh@icloud.com>
2024-09-06 15:33:34 +0200

    Created order and payment dtos

---
+++ src/main/java/com/ecommerce/project/payload/OrderDTO.java
@@ -0,0 +1,24 @@
+package com.ecommerce.project.payload;
+
+import com.ecommerce.project.model.Address;
+import com.ecommerce.project.model.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+    private Long id;
+    private String eamil;
+    private List<OrderItem> orderItems;
+    private LocalDate localDate;
+    private PaymentDTO payment;
+    private double totalAmount;
+    private String orderStatus;
+    private Address address;
+}
