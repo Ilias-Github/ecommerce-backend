b4584c76fd2546e5c36f324cc06499a917c458ec
Ilias <ilias.azagagh@icloud.com>
2024-09-10 10:17:02 +0200

    Added fields to oder

--- src/main/java/com/ecommerce/project/model/Order.java
+++ src/main/java/com/ecommerce/project/model/Order.java
@@ -7,33 +7,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "app_order")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     // TODO: is dit veld nodig gezien een order al gekoppeld is aan een user
     @Email
     @Column
     private String email;
-
-    @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    private List<OrderItem> orderItemList;
-
     private LocalDate orderDate;
+    private double totalAmount;
+    private String orderStatus;
 
     @OneToOne
     private Payment payment;
 
-    private double totalAmount;
-    private String orderStatus;
-
     @ManyToOne
     private Address address;
+
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<OrderItem> orderItemList = new ArrayList<>();
 }
